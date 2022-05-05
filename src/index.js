f6b2c751b098697beb76679ddc8ebacc8b6f96b4
lauradillonar <laura.dillon.ar@gmail.com>
2022-05-05 11:52:20 -0300

    Usando getDocs para obtener datos de la colección

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import { app, database } from './firebaseConfig.js';
 //import { getAuth, GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { getModularInstance } from '@firebase/util';
  
     const collectionRef = collection(database, "users");
 
-    const form = document.getElementById("formLogin");
+    const getDataButton = document.getElementById("getDocsButton");
+    getDataButton.addEventListener("click", getData());
 
+    const form = document.getElementById("formLogin");
     form.addEventListener("submit", loginForm);
+
+    function getData(){
+        getDocs(collectionRef)
+            .then((response)=>{
+                console.log(response.docs.map((item)=>{
+                    return {...item.data(), id: item.id};
+                }))
+            });
+    }
 
     function loginForm(event){
         console.log("loginForm");
@@ -36,3 +48,4 @@ import { collection, addDoc } from 'firebase/firestore';
             });
     }
 
+    
