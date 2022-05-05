d780fbd3e4fa0f119f05ff2276d6e7f1bbc1c20d
lauradillonar <laura.dillon.ar@gmail.com>
2022-05-05 16:41:26 -0300

    Update a doc

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import { app, database } from './firebaseConfig.js';
 //import { getAuth, GoogleAuthProvider, signInWithPopup } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { 
+    collection, 
+    addDoc, 
+    getDocs,
+    doc, 
+    updateDoc } from 'firebase/firestore';
 
  
     const collectionRef = collection(database, "users");
 
     const getDataButton = document.getElementById("getDocsButton");
-    getDataButton.addEventListener("click", getData());
+    getDataButton.addEventListener("click", getData);
+
+    const updateButton = document.getElementById("updateButton");
+    updateButton.addEventListener("click", updateDatum);
 
     const form = document.getElementById("formLogin");
     form.addEventListener("submit", loginForm);
@@ -17,6 +25,20 @@ import { collection, addDoc, getDocs } from 'firebase/firestore';
                 console.log(response.docs.map((item)=>{
                     return {...item.data(), id: item.id};
                 }))
+            });
+    }
+
+    function updateDatum(){
+        const docToUpdate = doc(database, "users","xDN895ajhsvxdv1c6ZAy");
+        updateDoc(docToUpdate, {
+            email: "laura3@confluenciacreativa.com",
+            password: "muybueno"
+        })
+            .then(()=>{
+                alert("Data Updated");
+            })
+            .catch((err)=>{
+                alert(err.message);
             });
     }
 
